d051ca664ed9b04a27da947ad22b42ababf9153f
Da-Nob <danielnoberto194@gmail.com>
2026-02-20 09:26:26 -0300

    inclusao do modelo curso

--- taua_timetabling/cadastro/models.py
+++ taua_timetabling/cadastro/models.py
@@ -6,6 +6,13 @@ class Professor(models.Model):
     nome = models.CharField(max_length=1024, null=False)
     email = models.EmailField(unique=True)
 
+
+class Curso(models.Model):
+    id =  models.AutoField(primary_key=True)
+    coordenador = models.ForeignKey(Professor, null=False, on_delete=models.DO_NOTHING)
+    nome = models.CharField(unique=True)
+    nivel = models.CharField()
+    
 class Disciplina(models.Model):
     id = models.AutoField(db_column='id', primary_key=True)
     carga_horaria = models.IntegerField()
