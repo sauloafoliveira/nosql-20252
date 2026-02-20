7b7f7f11515c14adaa593b833d3745844ab62710
Saulo Oliveira <saulo.freitas.oliveira@gmail.com>
2026-02-20 08:57:12 -0300

    Classe Professor

--- taua_timetabling/cadastro/models.py
+++ taua_timetabling/cadastro/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    id = models.IntegerField(db_column='siape', primary_key=True)
+    nome = models.CharField(max_length=1024, null=False)
+    email = models.EmailField(unique=True)
+
