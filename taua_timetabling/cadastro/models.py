46ec3c655c342c390eaa3f33841ee890e0c71de6
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2026-02-20 09:12:28 -0300

    feat: model Disicplina

--- taua_timetabling/cadastro/models.py
+++ taua_timetabling/cadastro/models.py
@@ -6,3 +6,9 @@ class Professor(models.Model):
     nome = models.CharField(max_length=1024, null=False)
     email = models.EmailField(unique=True)
 
+class Disciplina(models.Model):
+    id = models.AutoField(db_column='id', primary_key=True)
+    carga_horaria = models.IntegerField()
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    semestre = models.IntegerField()
+
